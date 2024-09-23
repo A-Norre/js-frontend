1581467544ac32d236d3ebb43044c648f9ccbb7a
susm92 <sucrosmrkovic92@gmail.com>
2024-09-23 14:44:20 +0200

    added frontend functionality for create

--- my-react-app/src/components/NewDocument.jsx
+++ my-react-app/src/components/NewDocument.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import '../style/document.css';
 
@@ -5,23 +6,45 @@ const NewDocument = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate('/');
+    
+    try {
+      const response = await fetch(`http://localhost:3030/data`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          content,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update document');
+      }
+
+      navigate('/');
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
     <div className="document-container">
       <div className="document-content">
-        <h2>Dokument {id}</h2>
-        <p>Skapa Nytt Dokument</p>
+        <h2>Skapa Nytt Dokument</h2>
       </div>
       <form className="document-form" onSubmit={handleSubmit}>
         <label htmlFor="title">Titel</label>
-        <input type="text" id="title" name="title" />
+        <input type="text" id="title" name="title" onChange={(e) => setTitle(e.target.value)}/>
 
         <label htmlFor="content">Innehåll</label>
-        <textarea id="content" name="content"></textarea>
+        <textarea id="content" name="content" onChange={(e) => setContent(e.target.value)}></textarea>
 
         <button type="submit">Skapa</button>
       </form>
